90a60b6f117db6a7b988ee5e8e54f7e7f449ee23
Anna Lypovenko <anna.lypovenko@gmail.com>
2020-11-05 23:31:09 +0200

    filters changed according to client's request to the left side + button + input outline none

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,7 @@
 function myFunction() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
+    setTimeout(function(){
+        document.getElementById('myPopup').style.display = 'none';
+    }, 2000);
 }
